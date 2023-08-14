1d65b0cafc86c91c47634d84bbb2caeb44d6bbfe
elias-caldas <EliasCaldas012@gmail.com>
2023-08-14 06:29:39 -0300

    UPDATE - Tratamento de erro

--- src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
+++ src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudopratico.estudoSPRING.repositories.UserRepository;
 import com.estudopratico.estudoSPRING.services.exceptions.DatabaseException;
 import com.estudopratico.estudoSPRING.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -46,9 +48,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
+
+        try{
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj){
