d839f298c22b044b0119e650e0910872a7f042f9
elias-caldas <EliasCaldas012@gmail.com>
2023-08-14 06:25:52 -0300

    DELETE - Tratamento de erro

--- src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
+++ src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.estudopratico.estudoSPRING.entities.User;
 import com.estudopratico.estudoSPRING.repositories.UserRepository;
+import com.estudopratico.estudoSPRING.services.exceptions.DatabaseException;
 import com.estudopratico.estudoSPRING.services.exceptions.ResourceNotFoundException;
 
 
@@ -30,7 +33,16 @@ public class UserService {
     }
 
     public void delete(Long id){
+        try{
         repository.deleteById(id);
+
+        }catch(EmptyResultDataAccessException e)
+    {
+        throw new ResourceNotFoundException(id);
+    }catch(DataIntegrityViolationException e){
+        throw new DatabaseException(e.getMessage());
+        
+    }
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/estudopratico/estudoSPRING/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.estudopratico.estudoSPRING.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
