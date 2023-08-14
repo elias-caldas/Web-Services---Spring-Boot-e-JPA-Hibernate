328a8572c150f07203dfaed3c143b2484f84ed58
elias-caldas <EliasCaldas012@gmail.com>
2023-08-14 06:05:59 -0300

    GET - tratamento de erro

--- src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
+++ src/main/java/com/estudopratico/estudoSPRING/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.estudopratico.estudoSPRING.entities.User;
 import com.estudopratico.estudoSPRING.repositories.UserRepository;
+import com.estudopratico.estudoSPRING.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
